5ff9f3f4fac1004143756bb4ef10c340f41fc49a
Greg Holthaus <gerg@gerg.me>
2018-09-19 22:33:55 -0700

    cleanup

--- k8s-rbac-view.sh
+++ k8s-rbac-view.sh
@@ -5,7 +5,6 @@
 
 # Main function
 main (){
-    #basic informations, hostname, date,...
     window "OptimisCorp Kubernetes RBAC Viewer" "green"
     append "`date`"
     addsep
@@ -36,34 +35,5 @@ main (){
 	[[ -s "${d}" ]] && append "No client key detected" || append "${d}"
     done
     endwin        
-
-    #memory usage
-#    window "Memory usage" "red"
-#    append_tabbed `cat /proc/meminfo | awk '/MemTotal/ {print "Total:" $2/1024}'` 2
-#    append_tabbed `cat /proc/meminfo | awk '/MemFree/ {print "Free:" $2/1024}'` 2
-#    endwin
-
-    #5 more used process ordered by cpu and memory usage
-#    window "Processus taking memory and CPU" "green"
-#    for i in `seq 2 6`; do
-#        append_tabbed "`ps ax -o pid,rss,pcpu,ucmd --sort=-cpu,-rss | sed -n "$i,$i p" | awk '{printf "%s: %smo:  %s%%" , $4, $2/1024, $3 }'`" 3
-#    done
-#    endwin
-
-    #get dmesg, log it then send to deskbar
-#    window "Last kernel messages" "blue"
-#    dmesg | tail -n 10 > /dev/shm/deskbar.dmesg
-#    append_file /dev/shm/deskbar.dmesg
-#    rm -f /dev/shm/deskbar.dmesg
-#    endwin
-
-    #a special manipulation to get net interfaces  and IP
-#    window "Inet interfaces" "grey"
-#    _ifaces=$(for inet in `ifconfig | cut -f1 -d " " | sed -n "/./ p"`; do ifconfig $inet | awk 'BEGIN{printf "%s", "'"$inet"'"} /adr:/ {printf ":%s\n", $2}'|sed 's/adr://'; done)
-#    for ifac in $_ifaces; do
-#        append_tabbed  "$ifac" 2
-#    done
-#    endwin
-}
 
 main_loop 0.5
