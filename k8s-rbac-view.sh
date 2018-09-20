d3fd1709764d90f778ec0f30f25fb6adb017c17a
Greg Holthaus <gerg@gerg.me>
2018-09-20 14:03:27 -0700

    add minikube conditional

--- k8s-rbac-view.sh
+++ k8s-rbac-view.sh
@@ -8,33 +8,50 @@ main (){
     window "OptimisCorp Kubernetes RBAC Viewer" "green"
     append "`date`"
     addsep
-    append "Context: `kubectl config current-context` | Namespace: `kubectl config get-contexts | grep -v "CURRENT" | awk '{print $5}' | sed '/^$/d'`" 
+    append "Context: `kubectl config current-context`" 
     endwin 
 
     window "Password Authentication Details" "blue"
-    K8S_USERS=(`kubectl config view --minify -o=json | jq '.users[].user.username'`)
-    K8S_AUTH_PW=(`kubectl config view --minify -o=json | jq '.users[].user.password'`)
+    K8S_USERS=(`kubectl config view --minify -o=json | jq -r '.users[].user.username'`)
+    K8S_AUTH_PW=(`kubectl config view --minify -o=json | jq -r '.users[].user.password'`)
     for a in ${K8S_USERS[@]}; do
 	for b in ${K8S_AUTH_PW[@]}; do
 	
-	[[ -s "${a}" ]] && append "No username/password auth detected" || append "${a} ${b}"
+	[[ "${a}" == "null" ]] && append "No username/password auth detected" || append "${a} ${b}"
     	done
     done
     endwin
     
     window "Client Certificates" "magenta"
-    K8S_CERT=(`kubectl config view --raw --minify -o=json | jq '.users[].user | with_entries(select(.key|match("client-certificate-data")))[]'`)
+    [[ $(kubectl config current-context) == "minikube" ]] && K8S_CERT=(`kubectl config view --raw --minify -o=json | jq -r '.users[].user | with_entries(select(.key|match("client-certificate")))[]'`) || \
+    K8S_CERT=(`kubectl config view --raw --minify -o=json | jq -r '.users[].user | with_entries(select(.key|match("client-certificate-data")))[]'`)
     for c in ${K8S_CERT[@]}; do
-	[[ -s "${c}" ]] && append "No client certificate detected" || append "${c}"
+	[[ "${#c}" == "0" ]] && append "No client certificate detected" || append "${c}"
     done
     endwin
 
     window "Client Keys" "yellow"
-    K8S_KEY=(`kubectl config view --raw --minify -o=json | jq '.users[].user | with_entries(select(.key|match("client-key-data")))[]'`)
+    [[ $(kubectl config current-context) == "minikube" ]] && K8S_KEY=(`kubectl config view --raw --minify -o=json | jq -r '.users[].user | with_entries(select(.key|match("client-key")))[]'`) || \
+    K8S_KEY=(`kubectl config view --raw --minify -o=json | jq -r '.users[].user | with_entries(select(.key|match("client-key-data")))[]'`)
     for d in ${K8S_KEY[@]}; do
-	[[ -s "${d}" ]] && append "No client key detected" || append "${d}"
+	[[ "${#d}" == "0" ]] && append "No client key detected" || append "${d}"
     done
     endwin        
+
+debug
 } 
 
+debug() {
+K8S_USERS_size=${#K8S_USERS}
+K8S_AUTH_PW_size=${#K8S_AUTH_PW}
+K8S_CERT_size=${#K8S_CERT}
+K8S_KEY_size=${#K8S_KEY}
+printf "K8S_USERS = ${K8S_USERS_size}\n"
+printf "K8S_AUTH_PW = ${K8S_AUTH_PW_size}\n"
+printf "K8S_CERT = ${K8S_CERT_size}\n"
+printf "K8S_KEY = ${K8S_KEY_size}\n"
+}
+
 main_loop 0.5
+
+
